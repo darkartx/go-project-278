07d72ed6d5f0f8ad939350248151bccc9b13780a
darkartx <darkartx@gmail.com>
2026-01-26 19:22:43 +0400

    *

--- api_test.go
+++ api_test.go
@@ -5,10 +5,12 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPingRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
