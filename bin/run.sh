dcfd1d938f4fbb5c6d4c642a6c2c4a8c2faffbf8
darkartx <darkartx@gmail.com>
2026-02-10 11:57:31 +0400

    *

--- bin/run.sh
+++ bin/run.sh
@@ -3,8 +3,8 @@ set -eu
 
 echo "[run.sh] Starting service"
 
-# echo "[run.sh] Running DB migrations"
-# goose -dir ./db/migrations postgres "${DATABASE_URL}" up
+echo "[run.sh] Running DB migrations"
+goose -dir ./db/migrations postgres "${DATABASE_URL}" up
 
 echo "[run.sh] Starting Go app"
 exec /app/bin/app s
